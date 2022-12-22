1006979449fd221f85c1a4bb0f506f3bfadfd1d5
Danail <d_krasimirov@yahoo.com>
2022-12-22 20:30:37 +0100

    the remote state

---
+++ gke-simple/provider.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.4"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+
+ 
+  credentials = var.credentials
+  region = var.region
+}
+
+terraform {
+ backend "gcs" {
+   bucket  = "terraformstate80"
+   prefix  = "terraform/state-simple"
+   
+ }
+}
+
+resource "random_integer" "int" {
+    min =100
+    max = 10000
+  
+}
